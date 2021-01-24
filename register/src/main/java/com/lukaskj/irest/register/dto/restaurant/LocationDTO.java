a2af38d95768517e10b333f1398d2569ffd8810b
Lukas Jacomin <lukasjacomin@gmail.com>
2021-01-24 02:04:43 -0300

    Refactoring

---
+++ register/src/main/java/com/lukaskj/irest/register/dto/restaurant/LocationDTO.java
@@ -0,0 +1,11 @@
+package com.lukaskj.irest.register.dto.restaurant;
+
+public class LocationDTO {
+
+   // public Long id;
+
+   public Double latitude;
+
+   public Double longitude;
+
+}
---
+++ register/src/main/java/com/lukaskj/irest/register/dto/restaurant/RestaurantDTO.java
@@ -0,0 +1,18 @@
+package com.lukaskj.irest.register.dto.restaurant;
+
+public class RestaurantDTO {
+   
+   public Long id;
+
+   public String ownerId;
+
+   public String companyId;
+
+   public String companyName;
+
+   public LocationDTO location;
+
+   public String createdAt;
+   
+   public String updatedAt;
+}
